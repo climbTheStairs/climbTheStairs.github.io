d72ce9e4f160c7641f23b112bbd64bf62a26eab5
climbTheStairs <yxtqwf@gmail.com>
2020-08-26 15:53:18 -0700

    Update main.js
    * Added `randStr()`
    * Changed indent size 2 --> 4

--- default/js/main.js
+++ default/js/main.js
@@ -1,5 +1,20 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 const create = (tag, props) => {
-  return Object.assign(document.createElement(tag), props);
+    return Object.assign(document.createElement(tag), props);
+};
+const randStr = (length = 20, charTypes = null) => {
+    const allChars = {
+        l: "abcdefghijklmnopqrstuvwxyz",
+        u: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+        n: "0123456789",
+    };
+    const chars = Object.values({
+        ...allChars,
+        ...charTypes,
+    }).map((x) => x || "").join("");
+    const res = [];
+    while (length--)
+        res.push(chars[~~(Math.random() * chars.length)]);
+    return res.join("");
 };
