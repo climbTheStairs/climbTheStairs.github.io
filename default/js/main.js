cd1f7deafcca08e5f6a91916f814a94e39bc7d88
climbTheStairs <51103593+climbTheStairs@users.noreply.github.com>
2020-10-25 12:15:29 -0700

    Update main.js
    Add $id, head, & body

--- default/js/main.js
+++ default/js/main.js
@@ -1,5 +1,8 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
+const $id = document.getElementById.bind(document);
+const head = document.head || document;
+const body = document.body || document;
 const create = (tag, props) => {
     return Object.assign(document.createElement(tag), props);
 };
