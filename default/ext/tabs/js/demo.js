a7e5e3b64317c9c49074aced283b377e1a2cdd5f
climbTheStairs <51103593+climbTheStairs@users.noreply.github.com>
2021-03-31 09:54:15 +0000

    Add files via upload

---
+++ default/ext/tabs/js/demo.js
@@ -0,0 +1,70 @@
+;(() => {
+    "use strict"
+    const createTabs = () => {
+        const tab1 = {
+            title: "One",
+            $content:
+                "We must secure the existence of our people and a " +
+                "future for trans children.",
+        }
+        const $penultimate = createFrag()
+        $penultimate.append(
+            "Penultimate",
+            create("br"),
+            "The spirit is willing but the flesh is weak",
+            create("br"),
+            "SCHADENFREUDE",
+            create("br"),
+            "3964 Elm Street and 1370 Rt. 21",
+            create("br"),
+            "The left hand does not know what the right hand is doing.",
+        )
+        const tab2 = {
+            title: "Two",
+            $content: $penultimate,
+        }
+        const tab3 = {
+            $content: "Chaos is a ladder. Petyr Baelish, Littlefinger.",
+        }
+        const tabs = [tab1, tab2, tab3]
+        const demoTabs = new Tabs("demo-tabs", tabs)
+        $body.append(demoTabs.$tabs)
+        return demoTabs
+    }
+    const setEvents = (tabs) => {
+        const $add = $("#add-tab")
+        // const $remove = $("#remove-tab")
+        const $getName = $("#get-tabs-name")
+        const $getLen = $("#get-tabs-len")
+        $add.onclick = () => {
+            const title = prompt("Tab title:")
+            const $content = prompt("Tab content:")
+            tabs.add({ title, $content })
+        }
+        /*
+        $remove.onclick = () => {
+            const is = prompt("Tabs to remove (separate by commas):")
+                .split(",")
+                .map(i => +i)
+            tabs.remove(...is)
+        }
+        //*/
+        $getName.onclick = () => alert(tabs.name)
+        $getLen.onclick = () => alert(tabs.length)
+    }
+    const demoTabs = createTabs()
+    setEvents(demoTabs)
+    console.log({ demoTabs })
+    $body.append(
+        new Tabs("extra-tabs", [{
+            title: "heres another 1",
+            $content: "cuz why not?",
+        }]).$tabs,
+        create("p", {
+            textContent: "This is a paragraph.",
+        }),
+        create("p", {
+            textContent: "This is another paragraph.",
+        }),
+    )
+})()
---
+++ default/ext/tabs/js/tabs.js
@@ -0,0 +1,82 @@
+;(() => {
+    "use strict"
+    window.Tabs = class {
+        constructor(name, tabs) {
+            this.$tabs = create("form", {
+                id: name,
+                className: "tabs",
+            })
+            this._name = name
+            this._length = 0
+            this.add(...tabs)
+        }
+        
+        _createTab({
+            title = `Tab ${this.length}`,
+            $content = "",
+        } = {}) {
+            const $tab = createFrag()
+            const id = `${this.name}-tab-${this.length}`
+            const $input = create("input", {
+                type: "radio",
+                id,
+                name: "tab",
+                checked: !this.length,
+            })
+            const $tabTitle = create("label", {
+                className: "tab-title",
+                htmlFor: id,
+                textContent: title,
+            })
+            const $tabContent = create("div", {
+                className: "tab-content",
+            })
+            
+            $tabContent.append($content)
+            $tab.append($input, $tabTitle, $tabContent)
+            
+            this._length++
+            
+            return $tab
+        }
+        
+        add(...tabs) {
+            for (const tab of tabs)
+                this.$tabs.append(this._createTab(tab))
+        }
+        
+        /**
+         * `remove` is very buggy and unnecessary. Do not use unless
+         * this functionality is needed.
+         *
+         * TOFIX:
+         * - Removing selected tab leaves no tab selected.
+         * - Tab ids are not updated after removing previous tabs.
+         *
+        remove(...is) {
+            const toRemove = is.filter(i => i < this.length).map((i) => {
+                const id = `#${this.name}-tab-${i}`
+                return [
+                    id,
+                    id + " + .tab-title",
+                    id + " + .tab-title + .tab-content",
+                ].join(", ")
+            })
+            if (!toRemove.length)
+                return
+            $$(toRemove.join(", ")).forEach($el => $el.remove())
+            this._length -= toRemove.length
+        }
+         */
+        
+        get name() { return this._name }
+        set name(name) {
+            throw new TypeError("Tabs name cannot be reassigned")
+        }
+        
+        get length() { return this._length }
+        set length(l) {
+            throw new TypeError("Tabs length cannot be reassigned")
+        }
+    }
+})()
