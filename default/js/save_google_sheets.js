f0aa02d3ff19d05631a59d98d97d712bc16b37f2
climbTheStairs <51103593+climbTheStairs@users.noreply.github.com>
2021-04-18 22:41:54 -0700

    create_save_google_sheets_js

---
+++ default/js/save_google_sheets.js
@@ -0,0 +1,24 @@
+window.stairz.saveGoogleSheets = () => {
+    "use strict"
+
+    const waffles = [...$$("table.waffle")]
+    if (waffles.length !== 1)
+        throw new TypeError("missing or multiple .waffle elements")
+    const [waffle] = waffles
+    if (waffle.$(":scope > thead").textContent.trim())
+        throw new TypeError("thead is not empty")
+
+    const table = [...waffle.$(":scope > tbody").children].map((row) => {
+        row = [...row.children]
+            .filter(cell => !cell.matches("th.row-headers-background"))
+            .map(cell => cell.textContent)
+        return row
+    })
+    const keys = table.shift()
+    const data = table.map((row) => {
+        const data = {}
+        row.forEach((cell, i) => data[keys[i]] = cell)
+        return data
+    })
+    return JSON.stringify(data, null, 4)
+}
