dbf44b72bfe9bf470eabfbdb3d811c61f828b94e
climbTheStairs <51103593+climbTheStairs@users.noreply.github.com>
2021-04-22 22:20:25 -0700

    Update scriptlets.js

--- priv/ubo_scriptlets.js
+++ priv/ubo_scriptlets.js
@@ -17,12 +17,12 @@
 // example.com##+js(lazy-load)
 ;(() => {
     const $$ = document.querySelectorAll.bind(document)
-    const lazyLoad = ($el) => {
+    const notLoaded = $$("img[data-src]")
+    notLoaded.forEach(($el) => {
         console.log($el.dataset.src)
         $el.src = $el.dataset.src
         delete $el.dataset.src
-    }
-    $$("img[data-src]").forEach($el => setTimeout(() => lazyLoad($el), 2000))
+    })
     console.log("+js(lazy-load)")
 })();
 
