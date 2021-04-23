e276eb5c7633d6845aac52cd47c0be34fdbad7e1
climbTheStairs <51103593+climbTheStairs@users.noreply.github.com>
2021-04-22 22:05:22 -0700

    Update scripts.js: Add lazy-load.js & stairz.js

--- priv/ubo_scriptlets.js
+++ priv/ubo_scriptlets.js
@@ -1,7 +1,7 @@
 /// ignore-target-blank.js
 /// alias itb.js
 // example.com##+js(ignore-target-blank)
-(() => {
+;(() => {
     const ignoreTargetBlank = ({ target }) => {
         const a = target.closest("a")
         if (!a || a.target !== "_blank")
@@ -9,4 +9,23 @@
         a.target = "_self"
     }
     document.addEventListener("click", ignoreTargetBlank)
-})()
+})();
+
+/// lazy-load.js
+/// alias lzl.js
+// example.com##+js(lazy-load)
+;(() => {
+    const $$ = document.querySelectorAll.bind(document)
+    $$("[data-src]").forEach($el => $el.src = $el.dataset.src)
+})();
+
+/// stairz.js
+/// alias cts.js
+// example.com##+js(stairz)
+;(() => {
+    const $script = document.createElement("script")
+    $script.src = "https://climbthestairs.org/default/js/main.js"
+    const $body = document.body || document
+    $body.append($script)
+    $script.remove()
+})();
